0c7b868e0fc64d39f91577c2dd1ff9ac15c194ab
wjplaud83 <wjplaud83@yahoo.com>
2015-09-07 10:48:24 -0400

    Create browserStryv.js

---
+++ browserStryv.js
@@ -0,0 +1,25 @@
+#pragma strict
+	 var isQuit = false;
+	
+	function OnMouseEnter()
+	{
+		renderer.material.color = Color.white;
+	}
+	
+	function OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+	}
+	
+	// Update is called once per frame
+	function OnMouseDown()
+	{
+		if(isQuit)
+		{
+			Application.Quit();
+		}
+		else
+		{
+			Application.OpenURL("https://soundcloud.com/stryvmusic/");
+		}
+	}
