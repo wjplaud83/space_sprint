f307393201ef83e66e6567f77746e453116c5ce6
wjplaud83 <wjplaud83@yahoo.com>
2015-09-07 11:03:08 -0400

    Create menuObject3.js

---
+++ menuObject3.js
@@ -0,0 +1,28 @@
+#pragma strict
+
+	var isQuit = false;
+
+	function OnMouseEnter()
+	{
+		renderer.material.color = Color.white;
+	}
+	
+	function OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+	}
+	
+	// Update is called once per frame
+	function OnMouseDown()
+	{
+		if(isQuit)
+		{
+			Application.Quit();
+		}
+		else
+		{
+			playerScript.Score = 0;
+			playerScript.Lives = 5;
+			Application.LoadLevel(3);
+		}
+	}
