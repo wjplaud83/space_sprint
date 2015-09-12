0354fe8dd1ae68ae00157fab5179763b2dace697
wjplaud83 <wjplaud83@yahoo.com>
2015-09-11 20:53:08 -0400

    error on 19

--- shipSpawnScript.js
+++ shipSpawnScript.js
@@ -2,11 +2,11 @@
 
 
 //spawn timers
-	var float = spawnThreshold;
-	var float = spawnDecrement;
+	var spawnThreshold;
+	var spawnDecrement;
 
 	//ship prefab
-	var GameObject = ship;
+	var ship;
 	
 	// Update is called once per frame
 	function Update ()
@@ -15,11 +15,11 @@
 	//random ranges for ship spawn
 		if(Random.Range(0, spawnThreshold) <=1)
 		{
-			//shipSpawnScript spawn Attribute random x positionVector3 pos = transform.position;
-			Vector3 = (transform.position);
+			//ShipSpawnScript spawn Attribute random x positionVector3 pos = transform.position;
+			Vector3 pos = transform.position;	//';'expected. insert semicolon at the end. (UCE0001)
 			Instantiate(ship, new Vector3(pos.x + Random.Range(-10, 11), pos.y, pos.z), Quaternion.identity);
 
-			spawnThreshold -= (spawnDecrement);
+			spawnThreshold -= spawnDecrement;
 			if(spawnThreshold < 10)
 			{
 				spawnThreshold = 10;
